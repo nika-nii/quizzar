21edb13b56eadd03089b74881c59b21a709a52f0
nika-nii <lordnik2905@gmail.com>
2020-06-27 23:26:33 +0300

    Update routing rules

--- backend/quizzar/quizzar/urls.py
+++ backend/quizzar/quizzar/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('/', include('quiz.urls')),
+    path('', include('quiz.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls'))
 ]
